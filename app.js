1e31fbe0472848803068f1fbfd954415fb9da26d
unknown <letrungthanh101@gmail.com>
2023-06-22 00:26:27 +0700

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello mai phen!');
+  res.send('Hello world!');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port!');
